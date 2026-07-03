71ac390a45cdbd1e26be38979ad1eeb243e32fdd
isnow-x <cone2119@naver.com>
2026-07-03 16:41:12 +0900

    feat: 다중선택 삭제 추가

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const app = express();
 const mysql = require("mysql2");
 const port = 3000;
 
-app.use(express.json()); // json -> object
-app.use(express.urlencoded({ extended: true })); // html form ->object
+app.use(express.json()); //json->object
+app.use(express.urlencoded({ extended: true })); //html form ->object
 
 let corsOptions = {
   origin: "*",
@@ -25,6 +25,7 @@ db.connect();
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
+
 app.get("/list", (req, res) => {
   const sqlQuery =
     "SELECT id, title, content, writer, DATE_FORMAT(date, '%Y-%m-%d') AS date FROM board;";
@@ -33,10 +34,11 @@ app.get("/list", (req, res) => {
     res.send(result);
   });
 });
+
 app.get("/view", (req, res) => {
   console.log(req.query.id);
   const id = req.query.id;
-  //const sqlQuery = `SELECT * FROM board WHERE id=${req.query.id};`;
+  // const sqlQuery = `SELECT * FROM board WHERE id=${req.query.id};`;
   const sqlQuery =
     "SELECT title, content, writer, DATE_FORMAT(date, '%Y-%m-%d') AS date FROM board WHERE id=?;";
   db.query(sqlQuery, [id], (err, result) => {
@@ -44,11 +46,45 @@ app.get("/view", (req, res) => {
     res.send(result);
   });
 });
+
 app.post("/write", (req, res) => {
   console.log(req.body);
   const { title, name, content } = req.body;
-  const sqlQuery = " insert into board (title,content,writer) values (?,?,?);";
+
+  const sqlQuery = "insert into board (title,content,writer) values (?,?,?);";
   db.query(sqlQuery, [title, content, name], (err, result) => {
+    if (err) throw err;
+    res.send(result);
+  });
+});
+
+app.post("/delete", (req, res) => {
+  console.log(req.body);
+  const { id } = req.body;
+
+  const sqlQuery = "DELETE FROM board WHERE id=?";
+  db.query(sqlQuery, [id], (err, result) => {
+    if (err) throw err;
+    res.send(result);
+  });
+});
+app.post("/deleteselect", (req, res) => {
+  console.log(req.body);
+  const { boardIdList } = req.body;
+
+  const sqlQuery = `delete from board where id in (${boardIdList})`;
+  db.query(sqlQuery, (err, result) => {
+    if (err) throw err;
+    res.send(result);
+  });
+});
+
+app.post("/update", (req, res) => {
+  console.log(req.body);
+  const { name, title, content, id } = req.body;
+
+  const sqlQuery = "UPDATE board SET writer=?, title=?, content=? WHERE id=?";
+  db.query(sqlQuery, [name, title, content, id], (err, result) => {
     if (err) throw err;
     res.send(result);
   });
